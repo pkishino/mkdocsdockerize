d516631a2dbca3d6217c758fc81dffa19bd49512
Patrick Kishino <patrick.a.kishino@gmail.com>
2020-10-21 12:48:33 +0900

    Serve command
    Serve will now serve the /page directory with mkdocs

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -3,8 +3,8 @@ set -e
 
 if [ "$1" = 'produce' ]; then
 	echo "to be implemented"
-elif ["$1" = 'serve' ]; then
-	echo "to be implemented"
-elif ["$1" = 'help' ]; then
+elif [ "$1" = 'serve' ]; then
+	exec mkdocs serve
+else
 	echo "Available commands are 'serve' (serve a packaged site) and 'produce' (package a site) "
 fi
