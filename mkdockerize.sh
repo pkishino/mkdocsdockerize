3f20069ae80479e84ebab6bc5f0638b37f7ead82
Patrick Kishino <patrick.a.kishino@gmail.com>
2020-10-21 13:13:00 +0900

    Serve works
    Serve will now work when mounting in a folder.
    mkdocs needed --dev-addr set to work in docker

--- mkdockerize.sh
+++ mkdockerize.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$1" = 'produce' ]; then
 	echo "to be implemented"
 elif [ "$1" = 'serve' ]; then
-	exec mkdocs serve
+	exec mkdocs serve --dev-addr=0.0.0.0:8000
 else
 	echo "Available commands are 'serve' (serve a packaged site) and 'produce' (package a site) "
 fi
