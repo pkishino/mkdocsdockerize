add2cfb94bc7724b5dbe91ed09126b9c44f50840
Patrick Kishino <patrick.a.kishino@gmail.com>
2020-10-21 15:37:08 +0900

    Trying to fix test for gitlab

--- test/test-produce.sh
+++ test/test-produce.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-set -e
 
+echo "produce test started"
 #simple test to check that the site was compressed and file is created
-docker run -v $(pwd)/test-site:/page --rm pkishino/mkdocsdockerize produce | gzip -9 >test.tar.gz
+docker run -v $CI_PROJECT_DIR/test/test-site:/page --rm $CI_REGISTRY_IMAGE:latest produce | gzip -9 >test.tar.gz
 if [ ! -f "test.tar.gz" ]; then
 	exit 1
 fi
-
 
 
--- test/test-serve.sh
+++ test/test-serve.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-set -e
 
+echo "serve test started"
 #simple test to check if the test page is up and available
-docker run -d -p 8000:8000 -v $(pwd)/test-site:/page --rm pkishino/mkdocsdockerize serve
-exit=$(curl --write-out '%{http_code}' --silent --output /dev/null localhost:8000)
-if [ $exit !== '200' ];then
+docker run -d -p 8000:8000 -v $CI_PROJECT_DIR/test/test-site:/page --rm $CI_REGISTRY_IMAGE:latest serve
+#need to wait for service to be up
+sleep 5
+exit=$(curl --write-out '%{http_code}' --silent --output /dev/null docker:8000)
+if [ $exit != '200' ];then
 	exit 1
 fi
-
